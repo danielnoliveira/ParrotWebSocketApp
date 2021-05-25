f9c6ccd744c49b6dc7629c3f5e557f8ad8a4eb10
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-05-24 23:55:55 -0300

    Interface inicial criada. Botão para enviar a mensagem, campo para o usuario escrever a mensagem, Container para mostrar as mensagems ao usuario sendo as deles no lado esquerdo e as do parrot no lado direito.

--- App.js
+++ App.js
@@ -8,47 +8,118 @@
 
 import React, {useState} from 'react';
 import {
+  KeyboardAvoidingView,
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 import {backgroundStyle} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const bRadiusDefaultInputs = 10;
 
 const App = () => {
   const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([
+    {text: 'Ola parrot', my: true},
+    {text: 'Ola humano', my: false},
+  ]);
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.select({ios: 'padding', android: 'height'})}>
       <StatusBar hidden />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <TextInput onChangeText={text => setMessage(text)} value={message} />
+        style={styles.scrollViewContainer}
+        contentContainerStyle={styles.messageContainer}>
+        {messages === null
+          ? []
+          : messages.map((m, index) => {
+              const side = m.my ? 'flex-start' : 'flex-end';
+              return (
+                <View style={[styles.messageBallon, {alignSelf: side}]}>
+                  <Text style={styles.messageText}>{m.text}</Text>
+                </View>
+              );
+            })}
       </ScrollView>
-    </SafeAreaView>
+      <View style={styles.inputsContainer}>
+        <TextInput
+          onChangeText={text => setMessage(text)}
+          value={message}
+          style={styles.inputText}
+          multiline={true}
+          placeholder="Sua mensagem aqui..."
+          placeholderTextColor="#b1b1b1"
+        />
+        <TouchableOpacity
+          style={styles.inputButton}
+          onPress={() => setMessage('')}>
+          <Icon name="send" color="white" size={30} />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputsContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: '5%',
+    paddingVertical: 14,
+    backgroundColor: '#c1c1c1',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputText: {
+    width: '85%',
+    backgroundColor: 'white',
+    color: 'black',
+    borderTopLeftRadius: bRadiusDefaultInputs,
+    borderBottomLeftRadius: bRadiusDefaultInputs,
+    paddingHorizontal: 18,
+    textAlign: 'justify',
+    fontSize: 20,
+    lineHeight: 22,
   },
-  highlight: {
-    fontWeight: '700',
+  inputButton: {
+    backgroundColor: 'rgb(255,156,120)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: bRadiusDefaultInputs,
+    borderBottomRightRadius: bRadiusDefaultInputs,
+    width: 55,
+  },
+  scrollViewContainer: {
+    backgroundColor: '#2d9',
+  },
+  messageContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 8,
+    paddingBottom: 10,
+  },
+  messageBallon: {
+    borderRadius: 20,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginBottom: 10,
+  },
+  messageText: {
+    fontWeight: '500',
+    fontSize: 22,
   },
 });
 
