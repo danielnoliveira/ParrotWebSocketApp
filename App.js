fb559b468669ac8b3b871eb7b392743ed81f5749
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-05-24 22:16:54 -0300

    Código base.

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {backgroundStyle} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar hidden />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <TextInput onChangeText={text => setMessage(text)} value={message} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
